86f4a279d307de7720475ff80536a4c910b675eb
Khazratkulov Ziyobek <khazratkulov.zt@phystech.edu>
2023-10-08 04:39:10 +0300

    modified

--- bank_personal_loan_modelling/infer.py
+++ bank_personal_loan_modelling/infer.py
@@ -1,38 +1,47 @@
 import os
+
 import pandas as pd
 from catboost import CatBoostClassifier
+from sklearn.metrics import accuracy_score as accuracy
+from sklearn.metrics import f1_score as f1
+from sklearn.metrics import precision_score as precision
+from sklearn.metrics import recall_score as recall
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
-DATA_PATH = os.path.dirname(os.getcwd()) + '/MLOps/dataset'
-MODEL_SAVE_PATH = os.path.dirname(os.getcwd()) + '/MLOps/models/model.cbm'
-TEST_PATH = DATA_PATH + '/test.csv'
-PREDICT_PATH = os.path.dirname(os.getcwd()) + '/MLOps/dataset/predict.csv'
+
+DATA_PATH = os.path.dirname(os.getcwd()) + "/MLOps/dataset"
+MODEL_SAVE_PATH = os.path.dirname(os.getcwd()) + "/MLOps/models/model.cbm"
+TEST_PATH = DATA_PATH + "/test.csv"
+PREDICT_PATH = os.path.dirname(os.getcwd()) + "/MLOps/dataset/predict.csv"
+
 
 def get_data(path_to_data):
     data = pd.read_csv(path_to_data)
     ssc = StandardScaler()
-    cols_to_scale = ['Age', 'Income', 'CCAvg', 'Mortgage']
+    cols_to_scale = ["Age", "Income", "CCAvg", "Mortgage"]
     data[cols_to_scale] = ssc.fit_transform(data[cols_to_scale])
-    data.drop(['ID', 'Experience', 'ZIP Code'], axis=1, inplace=True)
+    data.drop(["ID", "Experience", "ZIP Code"], axis=1, inplace=True)
     return data
 
+
 def split_data(data):
-    target_column = 'Personal Loan'
+    target_column = "Personal Loan"
     X = data.loc[:, data.columns != target_column]
     y = data.loc[:, target_column]
     return X, y
+
 
 def predicting_data(X_test, y_test):
     model = CatBoostClassifier()
     model.load_model(MODEL_SAVE_PATH)
     y_predict = model.predict(X_test)
-    print(f"Accuracy: {accuracy_score(y_test, y_predict)}")
-    print(f"Precision: {precision_score(y_test, y_predict)}")
-    print(f"Recall: {recall_score(y_test, y_predict)}")
-    print(f"F1-score: {f1_score(y_test, y_predict)}")
-    pd.DataFrame(y_predict, columns=['Personal Loan']).to_csv(PREDICT_PATH)
+    print(f"Accuracy: {accuracy(y_test, y_predict)}")
+    print(f"Precision: {precision(y_test, y_predict)}")
+    print(f"Recall: {recall(y_test, y_predict)}")
+    print(f"F1-score: {f1(y_test, y_predict)}")
+    pd.DataFrame(y_predict, columns=["Personal Loan"]).to_csv(PREDICT_PATH)
     print(f"===== Predictions saved to {PREDICT_PATH} =====")
+
 
 if __name__ == "__main__":
     test_data = get_data(TEST_PATH)
